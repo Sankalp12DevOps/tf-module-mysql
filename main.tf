112dfbe2f2716e46b963bf90ab324b62d213053d
Sankalp12DevOps <sravanthi388@gmail.com>
2023-04-27 21:25:53 +0530

    m

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "aws_db_instance" "mysql" {
   username                = "admin1"
   password                = "Roboshop@1"
   parameter_group_name    =  aws_db_parameter_group.robomysqlparamgroup.name
-  vpc_security_group_ids  =  [aws_security_group.allow_tls-robosop_mysql.name]
+  vpc_security_group_ids  =  [aws_security_group.allow_tls-robosop_mysql.id]
   skip_final_snapshot     = true
 }
 
