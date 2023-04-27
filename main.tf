01ee01aaaee0f34f382db34b7a2b2300628d5753
Sankalp12DevOps <sravanthi388@gmail.com>
2023-04-27 22:38:27 +0530

    m

--- main.tf
+++ main.tf
@@ -1,15 +1,16 @@
 resource "aws_db_instance" "mysql" {
-  allocated_storage       = 10
-  db_name                 = "mydb"
-  engine                  = "mysql"
-  engine_version          = "5.7"
-  instance_class          = "db.t3.micro"
-  username                = "admin1"
-  password                = "Password@123"
-  parameter_group_name    =  aws_db_parameter_group.robo_mysql_paramgroup.name
-  vpc_security_group_ids  =  [aws_security_group.allow_robosop_mysql.id]
-  db_subnet_group_name    =  aws_docdb_subnet_group.default.name
-  skip_final_snapshot     =  true
+  allocated_storage            = 10
+  db_name                      = "mydb"
+  engine                       = "mysql"
+  engine_version               = "5.7"
+  instance_class               = "db.t3.micro"
+  username                     = "admin1"
+  password                     = "Password@123"
+  parameter_group_name         =  aws_db_parameter_group.robo_mysql_paramgroup.name
+  vpc_security_group_ids       =  [aws_security_group.allow_robosop_mysql.id]
+  db_subnet_group_name         =  aws_docdb_subnet_group.default.name
+  skip_final_snapshot          =  true
+  manage_master_user_password  = false
 }
 
 resource "aws_db_parameter_group" "robo_mysql_paramgroup" {
