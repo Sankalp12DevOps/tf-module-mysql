6f0e8de8b0c815b62ded04d772fbdad562a53772
Sankalp12DevOps <sravanthi388@gmail.com>
2023-04-27 21:35:02 +0530

    m

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ resource "aws_db_instance" "mysql" {
   parameter_group_name    =  aws_db_parameter_group.robomysqlparamgroup.name
   vpc_security_group_ids  =  [aws_security_group.allow_tls-robosop_mysql.id]
   db_subnet_group_name    =  aws_docdb_subnet_group.default.name
-  skip_final_snapshot     = true
+  skip_final_snapshot     =  true
 }
 
 resource "aws_db_parameter_group" "robomysqlparamgroup" {
