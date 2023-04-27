230393a1a07a2d81ea9c5102791e18a11f040ce9
Sankalp12DevOps <sravanthi388@gmail.com>
2023-04-27 22:23:42 +0530

    m

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ resource "aws_db_instance" "mysql" {
   engine_version          = "5.7"
   instance_class          = "db.t3.micro"
   username                = "admin1"
-  password                = "RoboShop@1"
+  password                = "Password@123"
   parameter_group_name    =  aws_db_parameter_group.robo_mysql_paramgroup.name
   vpc_security_group_ids  =  [aws_security_group.allow_robosop_mysql.id]
   db_subnet_group_name    =  aws_docdb_subnet_group.default.name
