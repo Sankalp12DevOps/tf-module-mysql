8e8ea820ff22604d04d1d12d481d3d37ded507a5
Sankalp12DevOps <sravanthi388@gmail.com>
2023-04-27 21:59:08 +0530

    m

--- sg.tf
+++ sg.tf
@@ -1,4 +1,4 @@
-resource "aws_security_group" "allow_tls-robosop_mysql" {
+resource "aws_security_group" "allow_tls_robosop_mysql" {
   name        = "allow_tls"
   description = "Allow TLS inbound traffic"
   vpc_id      = data.terraform_remote_state.vpc.outputs.ROBO_VPC_ID
