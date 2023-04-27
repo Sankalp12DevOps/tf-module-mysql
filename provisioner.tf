38822ec5bc8b1b4108ef855809b33200c71070bb
Sankalp12DevOps <sravanthi388@gmail.com>
2023-04-27 22:22:15 +0530

    m

--- provisioner.tf
+++ provisioner.tf
@@ -1,16 +1,15 @@
-# resource "null_resource" "web" {
-#   # ...
+resource "null_resource" "web" {
+  # ...
+  provisioner "local-exec" {
+    command = <<EOF
+    curl -s -L -o /tmp/mysql.zip "https://github.com/stans-robot-project/mysql/archive/main.zip"
+    cd /tmp
+    unzip mysql.zip
+    cd mysql-main
+    mysql -h ${aws_db_instance.mysql.address} -uadmin -pRoboShop@1 <shipping.sql
 
-#   provisioner "local-exec" {
-#     command = <<EOF
-#     curl -s -L -o /tmp/mysql.zip "https://github.com/stans-robot-project/mysql/archive/main.zip"
-#     cd /tmp
-#     unzip mysql.zip
-#     cd mysql-main
-#     mysql -h ${aws_db_instance.mysql.address} -uadmin -pRoboShop@1 <shipping.sql
-
-#     EOF
-#   }
-# }
+    EOF
+  }
+}
 
 
