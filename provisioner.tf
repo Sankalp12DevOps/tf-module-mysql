f33fdb117cecc9ea07496ce22efaa0e61d94238e
Sankalp12DevOps <sravanthi388@gmail.com>
2023-04-28 07:27:17 +0530

    m

--- provisioner.tf
+++ provisioner.tf
@@ -1,12 +1,12 @@
 resource "null_resource" "web" {
+  depends_on = [aws_db_instance.mysql]
   provisioner "local-exec" {
     command = <<EOF
-    curl -s -L -o /tmp/mysql.zip "https://github.com/stans-robot-project/mysql/archive/main.zip
+    curl -s -L -o /tmp/mysql.zip "https://github.com/stans-robot-project/mysql/archive/main.zip"
     cd /tmp
     unzip mysql.zip
     cd mysql-main
     mysql -h ${aws_db_instance.mysql.address} -uadmin -pRoboshop1 <shipping.sql
-
     EOF
   }
 }
