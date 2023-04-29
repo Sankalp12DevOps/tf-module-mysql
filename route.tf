da2801176f5a73410c6e43e8aaea9bfd1361d324
Sankalp12DevOps <sravanthi388@gmail.com>
2023-04-29 11:34:06 +0530

    m

--- route.tf
+++ route.tf
@@ -1,13 +1,7 @@
 resource "aws_route53_record" "mysqlroute" {
-  zone_id = "Z01817288AYQJ8IWO87B"
-  name    = "www"
+  zone_id = data.terraform_remote_state.vpc.outputs.PUBLIC_HOSTED_ZONE_ID
+  name    = "mysql-${var.ENV}"
   type    = "CNAME"
+  records = [aws_db_instance.mysql.endpoint]
   ttl     = 5
-
-  weighted_routing_policy {
-    weight = 10
-  }
-
-  set_identifier = "dev"
-  records        = ["dev.example.com"]
 }
